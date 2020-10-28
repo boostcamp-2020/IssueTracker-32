14c0413633cadd523c9e2ea8acd24602c11bb527
JuHyeon Lee <leon6095@gmail.com>
2020-10-28 17:27:21 +0900

    [chore] ESLint, Prettier 설정

--- server/app.js
+++ server/app.js
@@ -1,11 +1,12 @@
 const express = require('express');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send("Hello!");
+  res.send('Hello!');
 });
 
 app.listen(port, () => {
-    console.log('listening at port ' + port + '...');
+  console.log(`listening at port ${port}...`);
 });
