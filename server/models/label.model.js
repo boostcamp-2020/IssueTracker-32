37c41d1e7b6a8e079445608f16e3dbf7f7c135fb
이주현 <leon6095@gmail.com>
2020-10-29 08:35:21 +0000

    [feat] label 모델 추가

---
+++ server/models/label.model.js
@@ -0,0 +1,19 @@
+
+module.exports = (sequelize , DataTypes)=> sequelize.define('label', {
+    name: {
+      type: DataTypes.STRING(50),
+      allowNull: false
+    },
+	description: {
+      type: DataTypes.STRING(100),
+    },
+	color: {
+      type: DataTypes.STRING(10),
+    },
+  }, {
+    timestamps: false,
+	charset: 'utf8',
+    collate: 'utf8_general_ci',
+});
+
+
