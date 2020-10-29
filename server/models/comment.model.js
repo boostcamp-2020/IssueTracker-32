084ffdb8c231129c214e662427a807494b33769a
이주현 <leon6095@gmail.com>
2020-10-29 08:34:39 +0000

    [feat] comment 모델 추가

---
+++ server/models/comment.model.js
@@ -0,0 +1,15 @@
+
+module.exports = (sequelize , DataTypes)=> sequelize.define('comment', {
+    mandatory: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false
+    },
+	detail: {
+      type: DataTypes.TEXT,
+    },
+  }, {
+    timestamps: true,
+	underscored: true,
+	charset: 'utf8',
+    collate: 'utf8_general_ci',
+});
