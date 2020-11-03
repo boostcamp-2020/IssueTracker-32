1288576ea1ec4708430bbc4dcafecd45a49f1c74
wseungjin <bose1021@naver.com>
2020-11-03 16:12:46 +0900

    [chore] page alias 설정

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -22,6 +22,7 @@ module.exports = {
     extensions: ['*', '.js', '.jsx'],
     alias: {
       '@components': path.resolve(__dirname, 'src/components/'),
+      '@pages': path.resolve(__dirname, 'src/pages/'),
     },
   },
   output: {
