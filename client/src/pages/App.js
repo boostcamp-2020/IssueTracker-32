063c74577eaa037f57b727941f496ce7d56a8ec6
wseungjin <bose1021@naver.com>
2020-11-03 16:13:58 +0900

    [feat] 라우터 추가해 라우터에 맞는 페이지로 이동 가능
    + react-router-dom 이용
    + 현재 로그인 페이지와 이슈 페이지로 이동 가능

--- client/src/pages/App.js
+++ client/src/pages/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import IssuePage from './IssuePage';
 import LoginPage from './LoginPage';
 
 const App = () => {
-  return <LoginPage />;
+  return (
+    <Switch>
+      <Route path="/" exact component={IssuePage} />
+      <Route path="/login" component={LoginPage} />
+    </Switch>
+  );
 };
 
 export default App;
