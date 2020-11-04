1aa43a7288b92285e435cefee2b2b3bf6a038fb9
handhand <sohnjunior@naver.com>
2020-11-04 16:15:44 +0900

    [feat] 소셜 로그인 토큰 저장 구현
    + 로그인성공 시 홈 페이지로 redirect
    + 토큰은 local storage 에 저장

--- client/src/pages/App.js
+++ client/src/pages/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import IssuePage from './IssuePage';
 import LoginPage from './LoginPage';
-import SocialPage from './SocialPage';
+import SaveJWTPage from './SaveJWTPage';
 
 const App = () => {
   return (
     <Switch>
       <Route path="/" exact component={IssuePage} />
       <Route path="/login" component={LoginPage} />
-      <Route path="/social" component={SocialPage} />
+      <Route path="/social" component={SaveJWTPage} />
     </Switch>
   );
 };
