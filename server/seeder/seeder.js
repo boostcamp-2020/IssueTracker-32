b9a91f54a86f1cd54b5f789894a403547bfc9c4e
JuHyeon Lee <leon6095@gmail.com>
2020-11-05 17:39:20 +0900

    [fix] issue data seeding 시 label과의 association 추가

--- server/seeder/seeder.js
+++ server/seeder/seeder.js
@@ -26,18 +26,34 @@ module.exports = async () => {
         }
     ]);
 
-    await Issue.bulkCreate([
-        {
-            is_open: 1,
-            title: 'test issue 1',
-            milestone_id: 1,
-            author_id: 1
-        },
-        {
-            is_open: 1,
-            title: 'test issue 2',
-            milestone_id: 2,
-            author_id: 2
-        }
-    ]);
+    const label_api = await Label.create({
+        name: "API",
+        description: "API develop",
+        color: "#000000"
+    });
+
+    const label_feature = await Label.create({
+        name: "Feature",
+        description: "New Feature",
+        color: "#FFFFFF"
+    });
+
+    const issue_test1 = await Issue.create({
+        is_open: 1,
+        title: 'test issue 1',
+        milestone_id: 1,
+        author_id: 1
+    });
+
+    const issue_test2 = await Issue.create({
+        is_open: 1,
+        title: 'test issue 2',
+        milestone_id: 2,
+        author_id: 2
+    })
+
+    await issue_test1.addLabel(label_api);
+    await issue_test1.addLabel(label_feature);
+    await issue_test2.addLabel(label_api);
+
 }
