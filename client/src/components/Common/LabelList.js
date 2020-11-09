7665308473db65effae9edb24a1ddad50603c435
wseungjin <bose1021@naver.com>
2020-11-09 12:59:19 +0900

    [feat] LabelList 컴포넌트 생성

---
+++ client/src/components/Common/LabelList.js
@@ -0,0 +1,20 @@
+import React, { useEffect } from 'react';
+import styled from 'styled-components';
+import LabelItem from './LabelItem';
+
+const LabelListContainer = styled.div`
+  display: flex;
+`;
+
+const LabelList = (props) => {
+  const labelList = props.labelList;
+  return (
+    <LabelListContainer>
+      {labelList.map((label, i) => {
+        return <LabelItem label={label} />;
+      })}
+    </LabelListContainer>
+  );
+};
+
+export default LabelList;
