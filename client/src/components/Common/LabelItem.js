0aad17a253c0f1f3844111275d2d11a19018e0c0
wseungjin <bose1021@naver.com>
2020-11-10 11:02:33 +0900

    [feat] 폰트 컬러 배경색에 맞게 계산

--- client/src/components/Common/LabelItem.js
+++ client/src/components/Common/LabelItem.js
@@ -13,7 +13,17 @@ const ItemWrapper = styled.div`
 const NameText = styled.p`
   padding: 2px 5px;
   font-weight: 500;
+  color: ${(props) => makeTextColor(props.colorStyle.backgroundColor)};
 `;
+
+const makeTextColor = (backgroundString) => {
+  const redColor = parseInt(backgroundString.substring(1, 3), 16);
+  const greenColor = parseInt(backgroundString.substring(3, 5), 16);
+  const blueColor = parseInt(backgroundString.substring(5, 7), 16);
+
+  const luminance = (0.299 * redColor + 0.587 * greenColor + 0.114 * blueColor) / 255;
+  return luminance > 0.5 ? '#000000' : '#ffffff';
+};
 
 const LabelItem = (props) => {
   const id = props.label.id;
@@ -26,7 +36,7 @@ const LabelItem = (props) => {
 
   return (
     <ItemWrapper colorStyle={colorStyle}>
-      <NameText> {name} </NameText>
+      <NameText colorStyle={colorStyle}> {name} </NameText>
     </ItemWrapper>
   );
 };
