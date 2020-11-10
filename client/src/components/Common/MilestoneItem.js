ff730d596f57bdceac5f69ceb8a81bc3a288fe1d
wseungjin <bose1021@naver.com>
2020-11-10 16:34:30 +0900

    [feat] 마일스톤 아이템 컴포넌트 생성

---
+++ client/src/components/Common/MilestoneItem.js
@@ -0,0 +1,31 @@
+import React, { useEffect } from 'react';
+import styled from 'styled-components';
+import { GoMilestone } from 'react-icons/go';
+
+const ItemWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 0px 3px;
+`;
+
+const NameText = styled.p`
+  padding: 0px 5px;
+  color: #868e96;
+  font-size: 12px;
+`;
+
+const MilestoneItem = (props) => {
+  const milestone = props.milestone;
+  const name = props.milestone.title;
+  return milestone !== undefined ? (
+    <ItemWrapper>
+      <GoMilestone size={12} />
+      <NameText> {name} </NameText>
+    </ItemWrapper>
+  ) : (
+    <></>
+  );
+};
+
+export default MilestoneItem;
