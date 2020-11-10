25e48626d191018095fd90bf60902db83ae59c22
wseungjin <bose1021@naver.com>
2020-11-10 22:04:43 +0900

    [feat] 필터 버튼에 모달 적용

--- client/src/components/Common/FilterButton.js
+++ client/src/components/Common/FilterButton.js
@@ -1,13 +1,15 @@
-import React, { useEffect } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import styled from 'styled-components';
 import { GoTriangleDown } from 'react-icons/go';
+import Modal from 'react-modal';
+import ModalContainer from './ModalContainer';
 
 const FilterButtonWrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
   border: none;
-  margin: 0px 5px;
+  margin: 0px 3px;
 `;
 
 const ButtonText = styled.p`
@@ -16,12 +18,52 @@ const ButtonText = styled.p`
 `;
 
 const FilterButton = (props) => {
+  const [modalIsOpen, setIsOpen] = useState(false);
+  const buttonElement = useRef(null);
+  const [topLocation, setTopLocation] = useState(0);
+  const [rightLocation, setRightLocation] = useState(0);
+
   const name = props.name;
+
+  const openModal = () => {
+    setIsOpen(true);
+  };
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
+  const setTopRightLocation = (bottom, right) => {
+    setTopLocation(bottom);
+    setRightLocation(right);
+  };
+
+  const customModalStyles = {
+    overlay: {
+      backgroundColor: 'none',
+    },
+    content: {
+      top: `${topLocation}px`,
+      left: 'auto',
+      right: `calc(100% - ${rightLocation}px)`,
+      bottom: 'auto',
+      padding: '0px',
+    },
+  };
+
+  useEffect(() => {
+    const { bottom, right } = buttonElement.current.getBoundingClientRect();
+    setTopRightLocation(bottom, right);
+  }, []);
   return (
-    <FilterButtonWrapper>
-      <ButtonText> {name}</ButtonText>
-      <GoTriangleDown />
-    </FilterButtonWrapper>
+    <div>
+      <FilterButtonWrapper ref={buttonElement} onClick={openModal}>
+        <ButtonText> {name}</ButtonText>
+        <GoTriangleDown />
+      </FilterButtonWrapper>
+      <Modal isOpen={modalIsOpen} onRequestClose={closeModal} style={customModalStyles}>
+        <ModalContainer closeModal={closeModal} />
+      </Modal>
+    </div>
   );
 };
 
