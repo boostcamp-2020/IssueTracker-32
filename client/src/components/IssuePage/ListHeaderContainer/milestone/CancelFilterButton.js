7f9aec6cc9f34cd1a45b191c9cdfe309a29e4f01
wseungjin <bose1021@naver.com>
2020-11-12 17:50:40 +0900

    [feat] milestone 필터링 관련된 컴포넌트

---
+++ client/src/components/IssuePage/ListHeaderContainer/milestone/CancelFilterButton.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useContext } from 'react';
+import styled from 'styled-components';
+import { FilterContext } from '@context/FilterContext';
+
+const ItemWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  border-top: solid 1px #dee2e6;
+  padding: 6px;
+`;
+
+const MainText = styled.p`
+  margin: 0px 0px 0px 10px;
+  font-weight: 500;
+`;
+
+const CancelFilterButton = (props) => {
+  const filterContext = useContext(FilterContext);
+  const cancelInfoString = 'not use milestone filtering';
+
+  const selectCancel = () => {
+    filterContext.setMilestone(null);
+  };
+
+  return (
+    <ItemWrapper onClick={selectCancel}>
+      <MainText> {cancelInfoString} </MainText>
+    </ItemWrapper>
+  );
+};
+
+export default CancelFilterButton;
