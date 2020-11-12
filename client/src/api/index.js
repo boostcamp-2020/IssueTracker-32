9f4a6dfc6b472071ca2ab225d330de91461083a4
wseungjin <bose1021@naver.com>
2020-11-12 14:11:14 +0900

    [feat] params를 이용한 fetchIssuesWithData 추가

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -3,6 +3,10 @@ import axios from 'axios';
 
 // TODO: 나중에 api 요청 추가해주세요!
 
+const transformBoolToInt = (boolean) => {
+  return boolean === true ? 1 : 0;
+};
+
 const fetchIssues = () => instance.get('/issue');
 
 const fetchLabelsCount = () => instance.get('/label/count');
@@ -13,4 +17,22 @@ const fetchIssuesCount = () => instance.get('/issue/count');
 
 const fetchUsers = () => instance.get('/user');
 
-export { fetchIssues, fetchIssuesCount, fetchLabelsCount, fetchMilestonesCount, fetchUsers };
+const fetchIssuesWithData = (data) =>
+  instance.get('/issue', {
+    params: {
+      is_open: transformBoolToInt(data.isOpen),
+      author_id: data.author,
+      labels: data.labels,
+      milestone_id: data.milestone,
+      assiginee: data.assiginee,
+    },
+  });
+
+export {
+  fetchIssues,
+  fetchIssuesCount,
+  fetchLabelsCount,
+  fetchMilestonesCount,
+  fetchUsers,
+  fetchIssuesWithData,
+};
