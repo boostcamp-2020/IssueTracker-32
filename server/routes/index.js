e8b7c63e256bac73b985104af82ecdb71fbb8697
Hangrae <meinewq@gmail.com>
2020-11-12 11:55:34 +0900

    [chore] swagger 설정

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('../configs/swagger')
+
+router.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
 const auth = require('./auth');
 router.use('/auth', auth);
 
